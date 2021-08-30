eb06032f1b50b5a598a9a890818f0b5b9e1d09c8
chetdoi2908 <onedirection19991999@gmail.com>
2021-08-31 03:24:53 +0700

    thêm Swagger

--- src/main/java/com/example/hoya/HoyaApplication.java
+++ src/main/java/com/example/hoya/HoyaApplication.java
@@ -3,8 +3,6 @@ package com.example.hoya;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -14,8 +12,6 @@ public class HoyaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HoyaApplication.class, args);
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        System.out.println(passwordEncoder.encode("123"));
     }
 
 }
