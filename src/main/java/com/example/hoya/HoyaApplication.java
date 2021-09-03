9768423f132c4f23cd2feec57f4f2c8095a77c13
hungnvse <hunguyen1499@gmail.com>
2021-09-04 00:56:11 +0700

    add deployable

--- src/main/java/com/example/hoya/HoyaApplication.java
+++ src/main/java/com/example/hoya/HoyaApplication.java
@@ -2,16 +2,22 @@ package com.example.hoya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableSwagger2
-public class HoyaApplication {
+public class HoyaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HoyaApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HoyaApplication.class);
     }
 
 }
