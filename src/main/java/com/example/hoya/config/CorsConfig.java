1171177c0906f83af29617b4c5484741ebead3bd
Okahama <48874863+Okahama@users.noreply.github.com>
2021-09-05 22:02:48 +0700

    sửa CorsConfig

--- src/main/java/com/example/hoya/config/CorsConfig.java
+++ src/main/java/com/example/hoya/config/CorsConfig.java
@@ -4,45 +4,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-//@Configuration
-//@EnableWebMvc
-//public class CorsConfig {
-//
-//    @Value("${allowed.origin}")
-//    private String allowedOrigin;
-//
-//    @Bean
-//    public WebMvcConfigurer getCorsConfiguration(){
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry
-//                        .addMapping("/**")
-//                        .allowedOrigins("http://14.161.47.36:8080")
-//                        .allowedMethods("*")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
-//}
 
 @Configuration
 public class CorsConfig {
 
-    public CorsConfig() {
-        super();
-    }
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer getCorsConfiguration(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("http://14.161.47.36:8080")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
